c20a06bf7fbb9e3f62f0127d69f30fc4b3e83e7b
Charles Briggs <chucklistmail+site.github.com@gmail.com>
2021-02-16 02:57:52 -0700

    remove Board::valid_space?

--- lib/board.rb
+++ lib/board.rb
@@ -4,13 +4,4 @@
 class Board
   def initialize
   end
-
-  def self.valid_space?(arr)
-    tf = false
-    if arr.length == 2
-      tf = true
-      tf = false if arr[0] < 0 || arr [0] > 7 || arr[1] < 0 || arr[1] > 7
-    end
-    tf
-  end
 end
